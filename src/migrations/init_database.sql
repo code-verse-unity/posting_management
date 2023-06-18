b256b7a5cf567715c95ef245a333d68a409a5eae
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-06-18 07:13:59 +0300

    fix(init_database.sql): correct the id field

--- src/migrations/init_database.sql
+++ src/migrations/init_database.sql
@@ -9,18 +9,18 @@
 create database posting_management_db;
 drop database posting_management_db;
 CREATE TABLE place (
-	id integer NOT NULL,
+	id serial primary key,
 	name character varying(255) NOT NULL,
 	province character varying(255) NOT NULL
 );
 CREATE TABLE employee (
-	id integer NOT NULL,
+	id serial primary key,
 	civility character varying(4) NOT NULL,
 	last_name character varying(255) NOT NULL,
 	first_name character varying(255) NOT NULL,
 	email character varying(255) NOT NULL,
 	job character varying(255) NOT NULL,
-	place_id integer,
+	place_id integer NOT NULL UNIQUE,
 	CONSTRAINT fk_place FOREIGN KEY (place_id) REFERENCES "place"(id) ON DELETE CASCADE
 );
 create table posting(
