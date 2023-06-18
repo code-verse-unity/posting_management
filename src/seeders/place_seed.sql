088e31f07f958d9f15c18227b5f620b920e6f8a2
Rasolomanana Herimanitra Olivier <herimanitraolivierr@gmail.com>
2023-06-18 09:02:12 +0300

    chore: add place seed examples

---
+++ src/seeders/place_seed.sql
@@ -0,0 +1,14 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  olivier
+ * Created: Jun 18, 2023
+ */
+
+INSERT INTO "place"(name, province)
+    VALUES
+        ('Analakely','Antananarivo'),
+         ('67ha','Antananarivo')
+;
