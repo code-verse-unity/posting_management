97c0436651ba40f28218e358ac6381181338be6a
Fitia Ismael Rakotondrazaka <ismaelrakotondrazaka@gmail.com>
2023-06-28 14:46:26 +0300

    fix(PostingView.java): Make the postingListJTable columns not editable

--- src/views/PostingView2.java
+++ src/views/PostingView2.java
@@ -552,7 +552,22 @@ public class PostingView2 extends javax.swing.JPanel {
             new String [] {
                 "Id", "Employée", "Lieu actuel", "Lieu d'affectation", "Date d'affection", "Date de prise de service"
             }
-        ));
+        ) {
+            Class[] types = new Class [] {
+                java.lang.Integer.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
         postingListJTable.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 postingListJTableMouseClicked(evt);
